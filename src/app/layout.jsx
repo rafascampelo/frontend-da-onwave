ec163a548074afa9accc2a0923a87edee2bcb9c6
rafaaacampelo <rafaccdfranca@gmail.com>
2024-08-24 19:56:09 -0300

    commit da ultima atualização

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,15 +1,24 @@
 
 import "./globals.css";
+import { Inter } from 'next/font/google'
+ 
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  style: 'normal',
+  
+})
 
 export const metadata = {
   title: "Onwave",
   description: "APP para a gestão do seu comercio",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout ({ children }) {
+
   return (
-    <html lang="pt-br">
-      <body>{children}</body>
+    <html lang="pt-br" >
+      <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -19,41 +19,41 @@ export default function Home() {
               priority/>
             </div>
             <div class="mt-0">
-        <form className="flex items-center justify-center flex-col">
+        <form class="flex items-center justify-center flex-col">
           <input
             id="email"
             type="text"
             placeholder="Email"
-            class=" w-[250px] h-[38px] bg-white rounded-[19px]  text-black/50 text-[15px] font-bold font-['Judson'] pl-10 pr-10
+            class=" w-[250px] h-[38px] bg-white rounded-[19px]  text-black/50 text-[15px] font-bold  pl-10 pr-10
             shadow focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500"/>
  
           <input
             id="senha"
             type="password"
             placeholder="Senha"
-            class=" mt-3 w-[250px] h-[38px] bg-white rounded-[19px]  text-black/50 text-[15px] font-bold font-['Judson'] pl-10 pr-10
+            class=" mt-3 w-[250px] h-[38px] bg-white rounded-[19px]  text-black/50 text-[15px] font-bold  pl-10 pr-10
             shadow focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500"/>
  
         </form>
 
       </div>
 
-     <a href="https://www.remove.bg/pt-br/upload" class=""> <div class="mt-2 w-[232px] h-[25px] text-center text-black/70 text-[13px] font-bold font-['Judson']">
+     <a href="https://www.remove.bg/pt-br/upload" class=""> <div class="mt-2 w-[232px] h-[25px] text-center text-black/70 text-[13px] font-bold ">
       Esqueci a minha senha</div>
       </a>
 
       <div class=" mt-10 w-[232px] h-[51px] text-center">
-        <span class="text-black text-[13px] font-bold font-['Judson']">Desculpe, sua</span>
-        <span class="text-[#ff0000] text-[13px] font-bold font-['Judson']"> senha</span>
-        <span class="text-black text-[13px] font-bold font-['Judson']"> ou </span>
-        <span class="text-[#ff0000] text-[13px] font-bold font-['Judson']">login</span>
-        <span class="text-black text-[13px] font-bold font-['Judson']"> está errado<br />por favor tente novamente.</span>
+        <span class="text-black text-[13px] font-bold ">Desculpe, sua</span>
+        <span class="text-[#ff0000] text-[13px] font-bold "> senha</span>
+        <span class="text-black text-[13px] font-bold "> ou </span>
+        <span class="text-[#ff0000] text-[13px] font-bold ">login</span>
+        <span class="text-black text-[13px] font-bold "> está errado<br />por favor tente novamente.</span>
       </div>
 
       
  
-  <Link href="cadastro/users.jsx" class="mt-20 h-[50px]  justify-center items-center flex
-  px-8 py-4 bg-[#0f1cf3] rounded shadow gap-2.5 text-white text-sm font-bold font-['Judson'] leading-[14px] "> 
+  <Link href="/register" class="mt-20 h-[50px]  justify-center items-center flex
+  px-8 py-4 bg-[#0f1cf3] rounded shadow gap-2.5 text-white text-sm font-bold  leading-[14px] "> 
   Entrar </Link>
    
 </main>
