4e14244b462370cdd717c9249817defc6de68ee3
unknown <Aluno@L3T11.acad.etecpoa>
2024-09-26 10:59:27 -0300

    nao funfou

--- src/app/(sells)/layout.jsx
+++ src/app/(sells)/layout.jsx
@@ -73,7 +73,7 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
         />
       </Head>
-      <div className="flex flex-col items-center justify-center bg-gray-100 min-h-screen">
+      <div className="flex flex-col items-center justify-center bg-gray-100 h-full w-full">
         {children}
         <div className="absolute top-5 left-3">
           <Link href="/homeScreen">
