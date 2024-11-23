aeeec12d8f58191711b214bc6f6e69e1d6f20cd4
rafaaacampelo <rafaccdfranca@gmail.com>
2024-11-23 18:29:13 -0300

    em processo

--- src/app/(screens)/boxScreen/page.jsx
+++ src/app/(screens)/boxScreen/page.jsx
@@ -4,13 +4,23 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useState } from "react";
 import PopUpEditar from "@/components/ui/PopUpEditar";
-import PopUpExcluir from "@/components/PopUpExcluir";
+import PopUpExcluir from "@/components/ui/PopUpExcluir";
 import PopUpNovoProduto from "@/components/ui/PopUpNovoProduto";
 import PopUpNovoLote from "@/components/ui/PopUpNovoLote";
 // PopUpEditar Component
 // PopUpExcluir Component
 // PopUpNovoProduto Component
 //PopUpNovoLote Component
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function BoxScreen() {
   const [isEditPopupVisible, setIsEditPopupVisible] = useState(false);
@@ -21,15 +31,7 @@ export default function BoxScreen() {
   return (
     <div className="flex flex-col h-screen">
       <div className="h-20 w-full bg-blue-700  flex items-center justify-center">
-        <div className="relative w-[330px] h-[50px] flex items-center justify-center top-[50px] bg-white shadow-lg rounded-lg">
-          <span className="text-lg font-semibold text-black text-center">
-            Meus Produtos
-          </span>
-        </div>
-      </div>
-
-      <div className="flex justify-center pt-10">
-        <div className="w-[272px] h-[50px] bg-white rounded-[46px] flex items-center relative shadow-xl">
+        <div className="relative w-[330px] h-[50px] flex items-center justify-center top-[40px] p-4 bg-white shadow-lg rounded-full">
           <input
             type="search"
             placeholder="Nome do produto"
@@ -38,7 +40,6 @@ export default function BoxScreen() {
           <Button
             size="sm"
             className="bg-transparent  absolute right-3 text-gray-500 cursor-pointe hover:bg-transparent"
-            onClick={() => console.log("oi")}
           >
             <Image
               src="/pesquisa.png" // Substitua pelo caminho do seu ícone de lupa
@@ -49,85 +50,120 @@ export default function BoxScreen() {
           </Button>
         </div>
       </div>
+      <div className="flex justify-center items-center p-4 mt-6 space-x-4">
+        <Dialog>
+          <DialogTrigger className="w-[200px] h-12 bg-white shadow-lg rounded-lg flex items-center justify-center">
+            <Image
+              src="/plus.png"
+              alt="Adicionar Produto"
+              className="w-8 h-8"
+              width={32}
+              height={32}
+            />
+            <span className="ml-2 text-sm font-bold text-blue-600">
+              Novos Produtos
+            </span>
+          </DialogTrigger>
+        </Dialog>
+        <DialogContent className="w-11/12 flex flex-col justify-between rounded-lg">
+          <DialogHeader>
+            <DialogTitle>Novo produto</DialogTitle>
+            <DialogDescription>Adicionar novo produto</DialogDescription>
+          </DialogHeader>
+          <input
+            type="text"
+            placeholder="Nome do produto"
+            className="mt-3 mb-4 w-[268.04px] h-[44.55px] bg-white rounded-[19px] text-black text-[15px] font-bold pl-10 pr-10 border-2 border-[#008fd7] focus:outline-none"
+          />
+          <h2 className="text-lg pb-3 text-[#5d5988] font-bold">
+            Qual o preço?
+          </h2>
+          <input
+            type="number"
+            placeholder="0000"
+            className="mb-6 w-[70px] h-[50px] bg-white rounded-lg border-2 border-[#008fd7] focus:outline-none justify-end items-center text-center text-[#61646b] text-sm font-normal"
+          />
+          <DialogFooter className="flex flex-row justify-evenly">
+            <button className="w-[268.04px] h-[44.55px] px-9 py-6 bg-[#008fd7] rounded-[40px] justify-center items-center gap-2 inline-flex text-center text-white text-lg font-bold leading-[18px] mb-6">
+              Pronto
+            </button>
+            <button className="w-[268.04px] h-[44.55px] px-9 py-6 bg-red-600 rounded-[40px] justify-center items-center gap-2 inline-flex text-center text-white text-lg font-bold leading-[18px] mb-6">
+              Cancelar
+            </button>
+          </DialogFooter>
+        </DialogContent>
 
-      <div className="relative max-w-[calc(100%-40px)] h-auto top-[50px] left-1/2 transform -translate-x-1/2 bg-white shadow-lg p-4 rounded-lg ">
-        <div className="flex flex-col ">
-          <div className="flex items-center">
-            <Image src="/caixabox.png" alt="Caixa Box" width={32} height={32} />
-            <div className="ml-4 flex-1">
-              <div className="text-lg font-semibold text-black">$Produto</div>
-              <div className="text-sm text-gray-600">
-                R$ 00.00 <br />
-                Lote 1, Quantidade: 100 <br />
-                Validade 00/00/0000
+        <button className="w-[200px] h-12 bg-white shadow-lg rounded-lg flex items-center justify-center">
+          <Image
+            src="/plus.png"
+            alt="Adicionar Lote"
+            className="w-8 h-8"
+            width={32}
+            height={32}
+          />
+          <span className="ml-2 text-sm font-bold text-blue-600">
+            Novo lote
+          </span>
+        </button>
+      </div>
+
+      <div className=" p-4 ">
+        <div className="items-center justify-center bg-white shadow-lg p-4 rounded-lg ">
+          <div className="flex flex-col">
+            <div className="flex items-center ">
+              <Image
+                src="/caixabox.png"
+                alt="Caixa Box"
+                width={32}
+                height={32}
+              />
+              <div className="ml-4 flex-1">
+                <div className="text-lg font-semibold text-black">$Produto</div>
+                <div className="text-sm text-gray-600">
+                  R$ 00.00 <br />
+                  Lote 1, Quantidade: 100 <br />
+                  Validade 00/00/0000
+                </div>
               </div>
-            </div>
-            <div>
-              <button className="flex flex-col">
-                <Image
-                  src="/check.png"
-                  alt="novolote"
-                  className="pb-4"
-                  width={24}
-                  height={24}
-                />
-              </button>
+              <div>
+                <button className="flex flex-col">
+                  <Image
+                    src="/check.png"
+                    alt="novolote"
+                    className="pb-4"
+                    width={24}
+                    height={24}
+                  />
+                </button>
 
-              {/* Botão para abrir pop-up de edição */}
-              <button onClick={() => setIsEditPopupVisible(true)}>
-                <Image src="/editar.png" alt="Editar" width={24} height={24} />
-              </button>
+                {/* Botão para abrir pop-up de edição */}
+                <button onClick={() => setIsEditPopupVisible(true)}>
+                  <Image
+                    src="/editar.png"
+                    alt="Editar"
+                    width={24}
+                    height={24}
+                  />
+                </button>
 
-              {/* Botão para abrir pop-up de exclusão */}
-              <button
-                onClick={() => setIsDeletePopupVisible(true)}
-                className="flex flex-col"
-              >
-                <Image
-                  src="/lixeira.png"
-                  alt="Excluir"
-                  className="pt-4"
-                  width={24}
-                  height={24}
-                />
-              </button>
+                {/* Botão para abrir pop-up de exclusão */}
+                <button
+                  onClick={() => setIsDeletePopupVisible(true)}
+                  className="flex flex-col"
+                >
+                  <Image
+                    src="/lixeira.png"
+                    alt="Excluir"
+                    className="pt-4"
+                    width={24}
+                    height={24}
+                  />
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-
-      <button
-        onClick={() => setIsNewProductPopupVisible(true)}
-        className="relative w-[330px] top-[100px] left-1/2 transform -translate-x-1/2 bg-white shadow-lg rounded-lg p-4 flex items-center"
-      >
-        <Image
-          src="/plus.png"
-          alt="Adicionar Produto"
-          className="w-8 h-8"
-          width={32}
-          height={32}
-        />
-        <span className="ml-4 text-lg font-bold text-blue-600">
-          Novos Produtos
-        </span>
-      </button>
-
-      <button
-        onClick={() => setIsNewLotePopupVisible(true)}
-        className="relative w-[330px] top-[110px] left-1/2 transform -translate-x-1/2 bg-white shadow-lg rounded-lg p-4 flex items-center"
-      >
-        <Image
-          src="/plus.png"
-          alt="Adicionar Produto"
-          className="w-8 h-8"
-          width={32}
-          height={32}
-        />
-        <span className="ml-4 text-lg font-bold text-blue-600">
-          Registrar novo lote
-        </span>
-      </button>
-
       <PopUpEditar
         isVisible={isEditPopupVisible}
         onClose={() => setIsEditPopupVisible(false)}
